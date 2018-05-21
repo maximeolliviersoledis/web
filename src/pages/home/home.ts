ae4f63483ee44916322eb23c416ac98dcc0cd925
Maxime Ollivier <maxime.ollivier@groupe-soledis.com>
2018-05-21 12:36:50 +0200

    first commit web

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  	selector: 'page-home',
+  	templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  	constructor(public navCtrl: NavController,
+  			 	private iab: InAppBrowser) {
 
-  }
+  	}
+
+  	ngOnInit(){
+
+    	const browser = this.iab.create('https://ionic.io','_self',{location:'no'}); 
+
+	}
 
 }
